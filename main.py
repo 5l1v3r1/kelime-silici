9546e9d21d4f6dd798bbf27ed24793e4287ae637
ZiksthemW <47642797+ZiksthemW@users.noreply.github.com>
2021-01-23 03:01:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,26 @@ with open(dosyaadi, 'r') as dosya: # Dosyamızı açıyoruz.
 
 silincek = str(input("Silinecek kelime: ")) # Dosyamızdan silinecek kelimeyi alıyoruz.
 dosyaverisi = dosyaverisi.replace(silincek, '') # Kelimeyi siliyoruz.
+print("-"*50)
 baska = str(input("E/H Sorusu\nBaşka kelime silmek ister misiniz?")) # Başka silmek istediği var mı diye soruyoruz.
+print("-"*50)
 
-if baska == "E": # Evet seçtiyse aşağıdaki kodu çalıştırıyoruz.
+while baska == "E":
     silincek2 = str(input("Silinecek kelime: ")) # Silinecek kelimeyi tekrar soruyoruz.
     dosyaverisi = dosyaverisi.replace(silincek2, '') # Kelimeyi tekrardan siliyoruz.
-    print("Başarıyla", silincek, "ve", silincek2, "silindi!") # Sildiğimize dair çıktı veriyoruz.
-else: # Hayır seçtiyse aşağıdaki kodu çalıştırıyoruz.
-    print("Başarıyla", silincek, "Silindi!") # Sildiğimize dair çıktı veriyoruz.
+    print("-"*50)
+    print("Başarıyla verilen kelime/kelimeler silindi!") # Sildiğimize dair çıktı veriyoruz.
+    print("-"*50)
+    baska = str(input("E/H Sorusu\nBaşka kelime silmek ister misiniz?"))
+
+print("-"*50)
+print("Başarıyla verilen kelime/kelimeler silindi!") # Sildiğimize dair çıktı veriyoruz.
+print("-"*50)   
+print("Programdan çıkılıyor..")
+#if baska == "E": # Evet seçtiyse aşağıdaki kodu çalıştırıyoruz.
+#    silincek2 = str(input("Silinecek kelime: ")) # Silinecek kelimeyi tekrar soruyoruz.
+#    dosyaverisi = dosyaverisi.replace(silincek2, '') # Kelimeyi tekrardan siliyoruz.
+#    print("Başarıyla", silincek, "ve", silincek2, "silindi!") # Sildiğimize dair çıktı veriyoruz.
 
 with open(dosyaadi, 'w') as dosya:
   dosya.write(dosyaverisi)
